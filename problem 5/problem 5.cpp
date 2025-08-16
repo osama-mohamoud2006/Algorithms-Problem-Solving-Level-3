1434201c538d6b202bed4bb3d3ead70eb37d34b4
osama mohamoud <wwwhdosama@gmail.com>
2025-08-16 21:51:15 +0300

    Auto commit: 2025-08-16 21:51:14

--- problem 5/problem 5.cpp
+++ problem 5/problem 5.cpp
@@ -18,16 +18,16 @@ void fill_array_with_random_numbers(int arr[3][3],int clo , int row) {
 
 int sum_of_col(int arr[3][3], int clo, int row) {
 	int sum = 0; 
-	for (int r = 0; r < row; r++) {
+	for (int r = 0; r < row; r++) 
 		sum += arr[r][clo];
-	}
+	
 	return sum; 
 }
 
 void fill_array2_with_sum(int arr2[3] , int arr[3][3] , int clo, int row) {
-	for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++) 
 		arr2[i] = sum_of_col(arr, i, row); 
-	}
+	
 }
 
 void print_res(int arr[3][3], int clo, int row) {
@@ -38,8 +38,8 @@ void print_res(int arr[3][3], int clo, int row) {
 		cout << endl;
 	}
 }
-void print_res(int arr2[3]) {
-	for (int i = 0; i < 3; i++) 
+void print_res(int arr2[3] , int length) {
+	for (int i = 0; i < length; i++) 
 		
 			cout << "the sum of col"<<i+1<<" = " << arr2[i] <<endl;
 		
@@ -58,5 +58,5 @@ int main() {
 
 	cout << "\nthe sum of each col is: \n";
 	fill_array2_with_sum(arr2, arr, 3, 3);
-	print_res(arr2);
+	print_res(arr2,3);
 }
