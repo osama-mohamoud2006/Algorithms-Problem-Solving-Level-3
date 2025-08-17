e575e09f59cc5b182c62e91a674ce987d3a8a2dc
osama mohamoud <wwwhdosama@gmail.com>
2025-08-18 00:56:38 +0300

    Auto commit: 2025-08-18 00:56:38

---
+++ problem 9/problem 9.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include<stdio.h>
+#include <cstdio>
+#include<ctime>
+ using namespace std; 
+int random(int from , int to){return rand()%(to-from+1)+from;}
+
+ void fill_matrix_with_random(int arr[3][3], short col , short row ){
+
+        for(int r=0; r<row; r++)
+        {
+            for(int c=0; c<col; c++){
+        
+                arr[r][c]=random(1,10);
+            }
+        }
+ }
+
+  void print_middle_row(int arr[3][3], short col , short row)
+ {
+    //r =1 --> middle
+            for(int c=0; c<col; c++){
+        
+                 printf("%d ", arr[1][c]);
+            }
+ }
+
+void print_middle_col(int arr[3][3], short col , short row)
+ {
+    //c =1 --> middle
+            for(int r=0; r<row; r++){
+        
+              printf("%d ", arr[r][1]);
+            }
+ }
+
+ void print(int arr[3][3], short col , short row){
+
+        for(int r=0; r<row; r++)
+        {
+            for(int c=0; c<col; c++){
+        
+              printf("%02d ",arr[r][c]);
+            }
+            cout<<endl;
+        }
+
+ }
+ 
+
+ int main()
+ {
+   srand((unsigned)time(NULL));
+   int arr[3][3];
+
+       fill_matrix_with_random( arr, 3 , 3 );
+ cout<<"\nthe matrix 1:\n";
+       print( arr, 3 , 3 );
+          
+       cout<<"\n\nthe middle row of the matrix is:\n";
+       print_middle_row( arr, 3 , 3 );
+
+     cout<<"\n\nthe middle col of the matrix is:\n";
+          print_middle_col( arr, 3 , 3 );
+
+    cout<<endl<<"\n";
+    return 0;
+
+ }
