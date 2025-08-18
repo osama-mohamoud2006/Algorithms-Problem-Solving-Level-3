c1bd87a9fac0ca3d18484aba0f34b255ba47c881
osama mohamoud <wwwhdosama@gmail.com>
2025-08-19 00:33:50 +0300

    Auto commit: 2025-08-19 00:33:50

---
+++ problem 20/problem 20.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+
+#include "E:\projects\my library\AllStuff.h"
+using namespace AllStuff;
+using namespace std;
+
+
+bool is_palindorme(short matrix[3][3], short row, short col)
+{
+    for (int r = 0; r < row; r++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+        // امشي علي كل صف وشوفه زي مقلوبه ولا لاء
+           if(matrix[r][c] != matrix[r][col-1-c] ) return false;
+        }
+    }
+    return true;
+}
+
+void print(short arr[3][3], short row, short col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+  
+    short matrix[3][3];
+    matrix[0][0] = 1;
+    matrix[0][1] = 2;
+    matrix[0][2] = 1;
+    matrix[1][0] = 5;
+    matrix[1][1] = 5;
+    matrix[1][2] = 5;
+    matrix[2][0] = 7;
+    matrix[2][1] = 3;
+    matrix[2][2] = 7;
+
+  
+    cout << "\nMatrix elements:\n";
+    print(matrix, 3, 3);
+
+    is_palindorme(matrix, 3, 3) ? cout << "\nPalindrome!\n" : cout << "\nNot Palindrome!\n";
+
+    cout << endl;
+    return 0;
+}
