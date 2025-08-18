6ef1dfec9f9692ba0aa98a17d0f1cf6e0e6ccfd9
osama mohamoud <wwwhdosama@gmail.com>
2025-08-19 01:05:10 +0300

    Auto commit: 2025-08-19 01:05:09

--- debugging/debugging.cpp
+++ debugging/debugging.cpp
@@ -1,20 +1,53 @@
-// debugging.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+
+#include "E:\projects\my library\AllStuff.h"
+using namespace AllStuff;
+using namespace std;
+
+
+bool is_palindorme(short matrix[3][3], short row, short col)
+{
+    for (int r = 0; r < row; r++)
+    {
+        for (int c = 0; c < col/2; c++)
+        {
+            // ???? ??? ?? ?? ????? ?? ?????? ??? ???
+            if (matrix[r][c] != matrix[r][col - 1 - c]) return false;//--> walk through 2columns only 
+            //the first c and the last c
+            //this happened bcz of the eq [col - 1 - c]
+        }
+    }
+    return true;
+}
+
+void print(short arr[3][3], short row, short col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << setw(3) << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    short matrix[3][3];
+
+    matrix[0][0] = 1; matrix[0][1] = 2; matrix[0][2] = 1;
+    matrix[1][0] = 5; matrix[1][1] = 5; matrix[1][2] = 5;
+    matrix[2][0] = 7; matrix[2][1] = 3; matrix[2][2] = 7;
+
+
+    cout << "\nMatrix elements:\n";
+    print(matrix, 3, 3);
+
+    is_palindorme(matrix, 3, 3) ? cout << "\nPalindrome!\n" : cout << "\nNot Palindrome!\n";
+
+    cout << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
