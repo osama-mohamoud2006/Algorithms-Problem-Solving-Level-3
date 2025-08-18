55553cbd8a684ef4082cee09fe3c6e5310232c5d
osama mohamoud <wwwhdosama@gmail.com>
2025-08-18 19:20:29 +0300

    Auto commit: 2025-08-18 19:20:29

--- problem 13/problem 13.cc
+++ problem 13/problem 13.cc
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
 
 void print(int arr[3][3], int row, int col)
 {
@@ -8,7 +8,7 @@ void print(int arr[3][3], int row, int col)
     {
         for (int i2 = 0; i2 < col; i2++)
         {
-            cout << arr[i][i2] << " ";
+            cout << setw(3) << arr[i][i2] << " ";
         }
         cout << endl;
     }
@@ -23,35 +23,39 @@ bool is_identity(int arr[3][3], int row, int col)
     {
         for (int j = 0; j < col; j++)
         {
+            // check the diagonal
             if (i == j)
             {
                 if (arr[i][i] != 1)
                     return false;
             }
 
-            else {if(arr[i][j]!=0) return false;}
+            // check the rest of elements
+            else
+            {
+                if (arr[i][j] != 0)
+                    return false;
+            }
         }
     }
 
-  
     return true;
 }
 
 int main()
 {
-    int arr[3][3];
-    arr[0][0] = 1;
-    arr[0][1] = 0;
-    arr[0][2] = 0;
-    arr[1][0] = 0;
-    arr[1][1] = 1;
-    arr[1][2] = 0;
-    arr[2][0] = 0;
-    arr[2][1] = 0;
-    arr[2][2] = 1;
+    int arr1[3][3];
+    arr1[0][0] = 1;  arr1[0][1] = 0;  arr1[0][2] = 0;
+    arr1[1][0] = 0;  arr1[1][1] = 1;  arr1[1][2] = 0;
+    arr1[2][0] = 0;  arr1[2][1] = 0;  arr1[2][2] = 1;
 
-    cout << "\nMatrix\n";
-    print(arr, 3, 3);
+    int arr2[3][3];
+    arr2[0][0]=1; arr2[0][1]=0; arr2[0][1]=1;
+    arr2[1][0]=0; arr2[1][1]=1; arr2[1][1]=0;
+    arr2[2][0]=1; arr2[2][1]=0; arr2[2][2]=0;
 
-    (is_identity(arr, 3, 3) == true) ? cout << "\nYes,matrix is identity!\n" : cout << "\nNO,the matrix isn't identity!\n";
+    cout << "\n  Matrix\n";
+    print(arr1, 3, 3);
+
+    (is_identity(arr1, 3, 3) == true) ? cout << "\nYes,matrix is identity!\n" : cout << "\nNO,the matrix isn't identity!\n";
 }
