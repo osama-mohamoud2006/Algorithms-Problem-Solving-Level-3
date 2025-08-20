1900aa6689908ba97eda2a8f454ac8115025e2c7
osama mohamoud <wwwhdosama@gmail.com>
2025-08-20 13:39:47 +0300

    Auto commit: 2025-08-20 13:39:46

--- problem 22/problem 22.cc
+++ problem 22/problem 22.cc
@@ -1,31 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void fib(short n, short prev1= 1,  short prev2 = 0){
+void fib(short n, short prev1 = 1, short prev2 = 0)
+{
 
-    if(n==0) return ; //base case
+    if (n == 0)
+        return; // base case
 
-     short res = prev1 + prev2;
-     cout<<res<<" ";
-     
-if(0!=n) fib( n=n-1, prev1 = prev2 - prev1, prev2 =res);
+    short res = prev1 + prev2;
+    cout << res << " ";
 
+    if (0 != n)
+        fib(n = n - 1, prev1 = prev2 - prev1, prev2 = res);
 }
 
-int fib2(short n, short prev1= 1,  short prev2 = 0){
+int fib2(short n, short prev1 = 1, short prev2 = 0)
+{
 
-    if(n==0) return 0 ; //base case
+    if (n == 0)
+        return 0; // base case
 
-     short res = prev1 + prev2;
-     cout<<res<<" ";
-     if(0!=n) return fib2( n=n-1, prev1 = prev2 - prev1, prev2 =res);
-
-
+    short res = prev1 + prev2;
+    cout << res << " ";
+    if (0 != n)
+        return fib2(n = n - 1, prev1 = prev2 - prev1, prev2 = res);
 }
 
-int main(){
+void fib3(short n, short prev1 = 1, short prev2 = 0)
+{
+    if (n == 0)
+        return;
+    else
+    {
+        short res = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = res;
+        cout << res << " ";
+        fib3(n = n - 1, prev1, prev2);
+    }
+}
+
+int main()
+{
     fib(10);
-    cout<<endl;
-     fib2(10);
+    cout << endl;
+    fib2(10);
+    cout << endl;
+    fib3(9);
 }
-
