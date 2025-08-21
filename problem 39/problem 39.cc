d01fc260ed9c28b00519c3b6deee832530b8bbe4
osama mohamoud <wwwhdosama@gmail.com>
2025-08-21 22:49:45 +0300

    Auto commit: 2025-08-21 22:49:44

---
+++ problem 39/problem 39.cc
@@ -0,0 +1,28 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include "E:\projects\my library\AllStuff.h"
+using namespace std;
+using namespace AllStuff;
+
+string join_string(vector<string> &vdata, string delim = " ")
+{
+    string res = " ";
+    for (int i=0; i<vdata.size(); i++ )
+    {
+
+        res+=vdata[i];
+        res+=delim;
+    }
+    res.pop_back();
+    return res;
+}
+
+int main(){
+    vector<string> vdata= {"osama","mohamoud","elsherif"};
+
+   cout<< join_string(vdata,"#")<<endl;
+
+}
